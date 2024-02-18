0f9f12d3be8aab12c5eb305837853b470325cfa7
DanielC19 <danysam1010@gmail.com>
2024-02-18 10:26:10 -0500

    Feature: Add visual functionality to Task component

--- nax-react/src/components/DailyList.js
+++ nax-react/src/components/DailyList.js
@@ -1,13 +1,51 @@
+import { useState } from 'react';
 import Task from "./Task";
 
+const defaultList = [
+  {
+    id: 1,
+    task: "Wake up",
+    hour: "6am",
+    completed: true
+  },
+  {
+    id: 2,
+    task: "Do exercise",
+    hour: "7am",
+    completed: true
+  },
+  {
+    id: 3,
+    task: "Read a book",
+    hour: "10am",
+    completed: false
+  }
+]
+
 function DailyList() {
+
+  const [list, setList] = useState(defaultList);
+
+  const handleCheckTask = event => {
+    const itemId = Number(event.target.id.substring(4));
+    const newList = list.map(item => {
+      if (itemId === item.id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
+    setList(newList);
+  }
+
   return (
     <div className="flex flex-col items-center bg-secondary p-8 rounded-lg w-96">
       <div className="text-center rounded-full bg-primary p-2 w-32 mb-6">
         <h3 className="text-2xl font-medium text-white">Daily</h3>
       </div>
       <div>
-        <Task task="Wake up early" hour="6am" />
+        {list.map(item => {
+          return <Task key={item.id} action={handleCheckTask} {...item} />
+        })}
       </div>
     </div>
   );
--- nax-react/src/components/Task.js
+++ nax-react/src/components/Task.js
@@ -1,30 +1,39 @@
 import proptype from 'prop-types';
 
 function Task({
-  task = 'Read a book',
-  hour = '12m'
+  id,
+  task,
+  hour,
+  completed,
+  action = () => {}
 }) {
 
   return (
     <span className="flex w-full mb-5">
       <span className="relative inline-flex items-center rounded-full cursor-pointer mr-3">
-        <input type="checkbox" className="todo-checkbox peer relative h-6 w-6 cursor-pointer appearance-none rounded-full transition-all hover:scale-105" />
+        <input
+          type="checkbox"
+          id={`todo${id}`}
+          checked={completed}
+          onChange={action}
+          className="todo-checkbox peer relative h-6 w-6 cursor-pointer appearance-none rounded-full transition-all hover:scale-105" />
         <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" strokeWidth={1}>
-            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd">
-            </path>
+            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
           </svg>
         </span>
       </span>
-      <p className="mr-1 w-full">{task}</p>
-      <p className="flex items-center underline stroke-black">{hour}</p>
+      <p className={`${completed && 'line-through'} mr-3 w-full`}>{task}</p>
+      <p className={`${completed && 'line-through'} flex items-center underline stroke-black`}>{hour}</p>
     </span>
   );
 }
 
 Task.propTypes = {
   task: proptype.string.isRequired,
-  hour: proptype.string.isRequired
+  hour: proptype.string.isRequired,
+  completed: proptype.bool.isRequired,
+  action: proptype.func
 }
 
 export default Task;
