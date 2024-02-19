1b84e4f85ce035105b2251bc03bc7c3dd7ed129f
M4C4NO5 <stufontrabajo@gmail.com>
2024-02-18 20:41:23 -0500

    Feature: Center the daily view

--- nax-react/src/pages/DailyHabits.js
+++ nax-react/src/pages/DailyHabits.js
@@ -1,7 +1,10 @@
 import DailyList from "../components/DailyList";
 
 function DailyHabits() {
-  return <DailyList />;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <DailyList />
+    </div>);
 }
 
 export default DailyHabits;
