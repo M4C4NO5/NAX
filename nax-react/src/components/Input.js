3e30cddbc32cd3874dd31a298250dc1357e6c7b7
DanielC19 <danysam1010@gmail.com>
2024-02-19 18:24:12 -0500

    Feature: Basic functionality to form

--- nax-react/src/components/Input.js
+++ nax-react/src/components/Input.js
@@ -1,12 +1,21 @@
-function Input({placeholder}) {
+function Input({
+  type = 'text',
+  value,
+  placeholder,
+  required = false,
+  action = () => {}
+}) {
   return (
     <div>
       <div className="relative">
         <input
-          type="text"
+          type={type}
           id={placeholder}
+          onChange={action}
+          value={value}
           className="border-b-2 py-1 border-[#AFC1D6] focus:outline-none bg-white focus:border-b-2 transition-colors duration-100 peer"
-          autocomplete="off"
+          required={required}
+          autoComplete="off"
         />
         <label
           htmlFor={placeholder}
@@ -14,8 +23,8 @@ function Input({placeholder}) {
         >
           {placeholder}
         </label>
-        </div>
       </div>
+    </div>
   );
 }
 
