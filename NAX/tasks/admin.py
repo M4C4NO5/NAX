f47eb62a8bc766974fa8f17611cd2e181b2e0109
M4C4NO5 <stufontrabajo@gmail.com>
2024-02-18 20:09:32 -0500

    Feature: Add basic migrations into database

--- NAX/tasks/admin.py
+++ NAX/tasks/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Habit, Task
 # Register your models here.
+admin.site.register(Habit)
+admin.site.register(Task)
--- NAX/tasks/models.py
+++ NAX/tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Habit(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    name  = models.CharField(max_length=20)
+    hour = models.CharField(max_length=10)
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Task(models.Model):
+    date = models.DateField(null=True)
+    checked = models.BooleanField(default=False)
+    habitId = models.ForeignKey(Habit, on_delete=models.CASCADE)
