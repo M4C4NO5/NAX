566e012f1e78160a2088f5b5e7cb6b14083e8b79
M4C4NO5 <stufontrabajo@gmail.com>
2024-02-21 12:36:14 -0500

    Feature: Explanation resetter

--- NAX/tasks/jobs.py
+++ NAX/tasks/jobs.py
@@ -8,10 +8,13 @@ from .models import Habit
 scheduler = BackgroundScheduler(timezone="UTC")
 scheduler.add_jobstore(DjangoJobStore(), "default")
 
+#It gives the instructions to be applied by the library
 @register_job(scheduler, "cron", day_of_week='sun', hour=0, minute=0)
 
 def reset_habit():
     Habit.objects.all().update(completed=False)
 
+#It allows to save the resetter
 register_events(scheduler)
 scheduler.start()
+
