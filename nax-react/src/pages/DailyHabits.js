c1ad65c9c9cdf147046300c56c1e1bab2616cf71
DanielC19 <danysam1010@gmail.com>
2024-02-20 22:18:28 -0500

    Feature: Progress bar on daily habits

--- nax-react/src/pages/DailyHabits.js
+++ nax-react/src/pages/DailyHabits.js
@@ -6,6 +6,7 @@ import { API_URL_TODO } from "../constants/constants";
 function DailyHabits() {
 
   const [list, setList] = useState([]);
+  const [percentage, setPercentage] = useState(0);
 
   useEffect(() => {
     axios.get(API_URL_TODO + "?format=json").then(
@@ -16,8 +17,24 @@ function DailyHabits() {
     );
   }, [])
 
+  useEffect(() => {
+    let total = list.length;
+    let counter = 0;
+    for (const item of list) {
+      if (item.completed) {
+        counter++;
+      }
+    }
+    setPercentage((counter * 100) / total);
+  }, [list])
+
   return (
     <main className="flex flex-col items-center justify-center h-screen">
+      {/* Progress bar */}
+      <h3 className="text-lg mb-3">Your progress today:</h3>
+      <div className="w-96 h-4 mb-6 bg-support rounded-full">
+        <div className="h-4 bg-primary rounded-full ease-in-out delay-75 duration-500" style={{width: `${percentage}%`}} />
+      </div>
       {/* List */}
       <DailyList list={list} setList={setList} />
     </main>
