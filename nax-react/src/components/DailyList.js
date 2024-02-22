ef10db11e8bb641e33105056cc37796046cf18ae
CursedDani <dfarangog@gmail.com>
2024-02-22 18:53:34 -0500

    Feature: Habit deletion functionality

--- nax-react/src/components/DailyList.js
+++ nax-react/src/components/DailyList.js
@@ -2,9 +2,13 @@ import { Link } from "react-router-dom";
 import Task from "./Task";
 import Button from './Button';
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { API_URL_TODO } from "../constants/constants";
+import { useState } from "react";
 
 function DailyList({ list, setList }) {
+
+  const navigate = useNavigate();
 
   const handleCheckTask = event => {
     const itemId = Number(event.target.id.substring(4));
@@ -19,6 +23,11 @@ function DailyList({ list, setList }) {
     setList(newList);
   }
 
+  const deletea = (itemid) => {
+    axios.delete(API_URL_TODO+itemid);
+    setList(list.filter(Task => Task.id !== itemid));
+  }
+
   return (
     <div className="flex flex-col items-center bg-secondary p-8 rounded-lg w-96">
       <div className="text-center rounded-full bg-primary p-5 w-60 mb-6">
@@ -26,7 +35,9 @@ function DailyList({ list, setList }) {
       </div>
       <div className="min-w-64 my-4">
         {list.map(item => {
-          return <Task key={item.id} action={handleCheckTask} {...item} />
+          return (<div><Task key={item.id} action={handleCheckTask} {...item} />
+          <Button text="Borrar" action={() => {
+            deletea(item.id)}} /></div>)
         })}
       </div>
       <Link to="newhabit">
