40288d555f6b99c1febe18a009ceb9e3a756c29e
M4C4NO5 <stufontrabajo@gmail.com>
2024-05-14 13:49:32 -0500

    Feature: Varint action added into Button

--- nax-react/src/components/Button.js
+++ nax-react/src/components/Button.js
@@ -3,11 +3,26 @@ function Button({
   text,
   id,
   type,
-  action = () => {}
+  action = () => {},
+  variant
 })
 {
+  let buttonClass = "bg-primary text-white text-xl py-3 px-8 rounded-xl"
+
+  if (variant=== "primary"){
+    buttonClass = "bg-primary text-white text-xl py-3 px-8 rounded-xl"
+  }if (variant=== "secondary"){
+    buttonClass = "bg-secondary text-white text-xl py-3 px-8 rounded-xl"
+  }else if(variant === "tertiary"){
+    buttonClass = "bg-tertiary text-white text-xl py-3 px-8 rounded-xl"
+  }else if(variant === "auxiliar"){
+    buttonClass = "bg-auxiliar text-white text-xl py-3 px-8 rounded-xl"
+  }else if(variant === "support"){
+    buttonClass = "bg-support text-white text-xl py-3 px-8 rounded-xl"
+  }
+
   return(
-    <button id={id} type={type} onClick={action} className="bg-primary text-white text-xl py-3 px-8 rounded-xl">
+    <button id={id} type={type} onClick={action} className={buttonClass}>
       {text}
     </button>
   )
@@ -17,6 +32,7 @@ Button.propTypes = {
   text: proptype.string.isRequired,
   type: proptype.string,
   action: proptype.func,
+  variant: proptype.oneOf(["primary","secondary","tertiary", "auxiliar", "support"])
 }
 
 export default Button
